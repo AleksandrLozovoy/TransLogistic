cd72e783ae160e27522823f19a3666e5b9551490
Aleksandr Lozovoy <lozovoialeksandr2019@gmail.com>
2022-10-19 12:28:45 +0300

    added scroll to sections (js)

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,5 @@
-// HeaderCroll
-
 $(function () {
+  // HeaderCroll
   let intro = $("#intro");
   let introH = intro.innerHeight();
   let header = $("#header");
@@ -23,4 +22,19 @@ $(function () {
       header.removeClass("header--dark");
     }
   }
+
+  // Scroll to sections
+  $("[data-scroll").on("click", function (event) {
+    event.preventDefault();
+
+    let scrollEl = $(this).data("scroll");
+    let scrollElPos = $(scrollEl).offset().top;
+
+    $("html, body").animate(
+      {
+        scrollTop: scrollElPos - 2.8 * headerH,
+      },
+      500
+    );
+  });
 });
